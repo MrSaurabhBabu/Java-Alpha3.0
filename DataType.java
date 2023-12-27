e366bcd390bcc5784825b68a69a259e3b7cabe58
Mr Saurabh Kumar <110296071+MrSaurabhBabu@users.noreply.github.com>
2023-12-27 19:20:30 +0530

    code

---
+++ DataType.java
@@ -0,0 +1,14 @@
+public class DataType {
+    public static void main(String args[]){
+        byte b=10;
+        System.out.println(b);
+        char ch='c';
+        System.out.println(ch);
+        boolean var=false;
+        float price=10;
+        int number=25;
+        //long
+        //double
+        short n=240;
+    }
+}
